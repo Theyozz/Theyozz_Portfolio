d27bbcd186d726f31fc1061f8522452e5a3a01ab
Théo VM <theomaurin875@gmail.com>
2023-05-19 11:00:13 +0200

    Head added

---
+++ src/components/Head/Head.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+
+const Heady = () =>  (
+    <Head>
+        <title>Vieilledent-Maurin Théo</title>
+    </Head>
+  );
+  
+  export default Heady;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
+import Head from '../components/Head/Head';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
-import Head from 'next/head';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
 const Home = () => {
   return (
     <Layout>
-      <Head>
-        <title>Theyozz's Portfolio</title>
-      </Head>
+      <Head />
       <Section grid>
         <Hero />
         <BgAnimation />
